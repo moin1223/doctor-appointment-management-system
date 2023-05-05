9102a16799f17f87338a92c9a4b4ac240772d942
moin1223 <moinpuccse@gmail.com>
2023-05-05 21:47:39 +0600

    add test

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div id="sidebar-wrapper">
     <div class="sidebar-heading text-center py-4  fs-4 fw-bold text-uppercase border-bottom"><i
-            class="fas fa-user-secret me-2"></i>Portfolio</div>
+            class="fas fa-user-secret me-2"></i>Doctor</div>
     <div class="list-group list-group-flush my-3">
         <a href="{{ route('dashboard') }}"class="list-group-item list-group-item-action bg-transparent active"><i
                 class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
